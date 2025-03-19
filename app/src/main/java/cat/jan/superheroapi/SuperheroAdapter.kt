b2f9aa12bb409d32016b3c7ec2cafc379948d64c
Jan Candalija <jcandalija.dam@institutcampalans.net>
2025-03-19 16:48:08 +0100

    Initial commit

---
+++ app/src/main/java/cat/jan/superheroapi/SuperheroAdapter.kt
@@ -0,0 +1,32 @@
+package cat.jan.superheroapi
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cat.jan.superheroapi.R
+
+class SuperheroAdapter(
+    var superheroList: List<SuperheroItemResponse> = emptyList(),
+    private val onItemSelected: (String) -> Unit
+) :
+    RecyclerView.Adapter<SuperheroViewHolder>() {
+
+    fun updateList(list: List<SuperheroItemResponse>) {
+        superheroList = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
+        return SuperheroViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(viewholder: SuperheroViewHolder, position: Int) {
+        viewholder.bind(superheroList[position],onItemSelected)
+    }
+
+    override fun getItemCount() = superheroList.size
+
+
+}
